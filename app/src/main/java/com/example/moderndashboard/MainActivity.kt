34e16ba900b31ee71708823b05bdc6114fe8e395
yjhfjh <agpan007@gmail.com>
2023-09-20 15:17:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/moderndashboard/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.moderndashboard
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val clothingCard = findViewById<CardView>(R.id.clothingCard)
+        clothingCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, ClothingActivity::class.java)
+            startActivity(intent)
+        }
+
+        val elec= findViewById<CardView>(R.id.elec)
+        elec.setOnClickListener {
+            val intent = Intent(this@MainActivity, ElectronicsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val casa= findViewById<CardView>(R.id.casa)
+        casa.setOnClickListener {
+            val intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val macy= findViewById<CardView>(R.id.macy)
+        macy.setOnClickListener {
+            val intent = Intent(this@MainActivity,PharmacyActivity::class.java)
+            startActivity(intent)
+        }
+
+        val gro= findViewById<CardView>(R.id.gro)
+        gro.setOnClickListener {
+            val intent = Intent(this@MainActivity,GroceriesActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+
+}
+
+
